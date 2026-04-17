34fe5a8d8c46e1f3c154afe75217266271826365
Rii-Dev <frasyiqpramana@gmail.com>
2026-04-17 17:36:19 +0700

    Fixing Routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,12 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-        'publisher' => CheckPublisher::class,
-    ]);
-        $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
+    $middleware->alias([
+            'publisher' => CheckPublisher::class,
+            'admin'     => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
